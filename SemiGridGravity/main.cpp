4f6b26312382ade6f91b7cf8c30006cfb7396b33
Calvin Akom <calvinakom@gmail.com>
2025-03-31 01:03:17 -0400

    more more more

--- SemiGridGravity/main.cpp
+++ SemiGridGravity/main.cpp
@@ -8,6 +8,16 @@ struct ScreenData
 	Vec2i pos = { 0,0 };
 };
 
+inline static raylib::Color lerpColor(raylib::Color color1, raylib::Color color2, float value)
+{
+	raylib::Color ret;
+	ret.r = (unsigned char)((1 - value) * color1.r + value * color2.r);
+	ret.g = (unsigned char)((1 - value) * color1.g + value * color2.g);
+	ret.b = (unsigned char)((1 - value) * color1.b + value * color2.b);
+	ret.a = (unsigned char)((1 - value) * color1.a + value * color2.a);
+	return ret;
+}
+
 int main()
 {
 	SetTraceLogLevel(LOG_FATAL);
@@ -16,7 +26,7 @@ int main()
 	raylib::Window window(800, 800, "Simulation");
 	Simulation sim;
 	sim.boundary = { {0,0},{100,100} };
-	sim.maxBoundary = { { -15,-15 }, { 115,115 } };
+	sim.maxBoundary = { { -25,-25 }, { 125,125 } };
 	//sim.maxBoundary = sim.boundary;
 	sim.cellSize = 1;
 	sim.gridSize = { 100,100 };
@@ -36,6 +46,10 @@ int main()
 		double mult = 4.5 * sqrt(dist) * dist;
 		particle.vel = 0.01 * Vec2d{ -mult * axis.y,mult * axis.x };
 	}
+
+	const raylib::Color color1 = { 0,0,0,255 };
+	const raylib::Color color2 = { 192,64,0,255 };
+	const raylib::Color color3 = { 255,255,0,255 };
 
 	float kernel[9] = {
 		1.0 / 16.0,
@@ -61,8 +75,6 @@ int main()
 
 		printf("%i: %i \n", frame++, count);
 		sim.Step();
-		BeginDrawing();
-		window.ClearBackground(BLACK);
 
 		Grid<ScreenData> screen(800, 800);
 		for (int x = 0; x < screen.X; x++)
@@ -81,7 +93,7 @@ int main()
 				continue;
 			screen[{(int)(sim.particles[i].pos.x * 8), (int)(sim.particles[i].pos.y * 8)}].mass += sim.particles[i].mass;
 		}
-		mutex drawMutex;
+		//mutex drawMutex;
 		raylib::Image screenImg(800, 800);
 		std::for_each(std::execution::par_unseq, screen.arr, screen.arr + screen.X * screen.Y, [&](const ScreenData& data) {
 			int x = data.pos.x;
@@ -90,35 +102,29 @@ int main()
 			//frac = frac * frac;
 			frac = sqrt(frac);
 			if (frac > 1) frac = 1;
-			Color color1 = { 0,0,0,255 };
-			Color color2 = { 192,64,0,255 };
-			Color color3 = { 255,255,0,255 };
-			Color color = BLACK;
+			raylib::Color color = BLACK;
 			if (frac < 0.5)
 			{
 				double lFrac = 2 * frac;
-				color.r = (unsigned char)((1 - lFrac) * color1.r + lFrac * color2.r);
-				color.g = (unsigned char)((1 - lFrac) * color1.g + lFrac * color2.g);
-				color.b = (unsigned char)((1 - lFrac) * color1.b + lFrac * color2.b);
+				color = lerpColor(color1, color2, lFrac);
 			}
 			else
 			{
 				double lFrac = 2 * (frac - 0.5);
-				color.r = (unsigned char)((1 - lFrac) * color2.r + lFrac * color3.r);
-				color.g = (unsigned char)((1 - lFrac) * color2.g + lFrac * color3.g);
-				color.b = (unsigned char)((1 - lFrac) * color2.b + lFrac * color3.b);
+				color = lerpColor(color2, color3, lFrac);
 			}
 			color.a = 255;
-			raylib::Vector2 screenPos((float)x, (float)y);
 			{
-				std::lock_guard<mutex> lock(drawMutex);
+				//std::lock_guard<mutex> lock(drawMutex);
 				screenImg.DrawPixel(x, y, color);
 			}
 			});
-		screenImg.KernelConvolution(kernel, 9);
+		//screenImg.KernelConvolution(kernel, 9);
 		raylib::Texture screenTex(screenImg);
-		screenTex.Draw();
 
+		BeginDrawing();
+		window.ClearBackground(BLACK);
+		screenTex.Draw();
 		EndDrawing();
 	}
 
